31036543b03a4eb50981c28ac2063b0d5b798cc3
Rick Mondal <124441126+rickmondal-2004@users.noreply.github.com>
2023-07-07 12:26:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+// Header toggle
 
+let MenuBtn = document.getElementById('MenuBtn');
+
+MenuBtn.addEventListener('click',function(e){
+  document.querySelector('body').classList.toggle('mobile-nav-active');
+  this.classList.toggle('fa-xmark')
+})
+
+//Typing effect
+let typed = new typed('.auto-input',{
+  Strings: ['Web Developer', 'Graphic Designer', 'Freelancer'],
+  typespeed: 100,
+  backspeed: 100,
+  backDelay: 2000,
+  loop: true,
+
+})
